6ec0915f878f61f2bd9987d42f573d9f7a2085fe
Luca Elena <36001301+LucaElena@users.noreply.github.com>
2022-05-24 22:49:18 +0300

    inceput controler programare

--- MvcPart/app/controllers/programare.php
+++ MvcPart/app/controllers/programare.php
@@ -2,9 +2,9 @@
 	class Programare extends Controller
 	{   
 
-        public function index($userName = "")
+        public function index($userName = "", $dataStart = "" )
 		{
-            
+            print_r("user = ".$userName ." data = ". $dataStart);
             $info['username'] =  $userName;
             $user = $this->model('UserModel');
             $programare = $this->model('ProgramareModel');
@@ -12,14 +12,48 @@
             $info['tabelProgram'] = "";
             $info['tabelSaptamanaProgram'] = "";
             
+            $week_start = "";
+            $week_end = "";
 
-            $day = date('w');
-            $week_start = date('Y-m-d 00:00:00', strtotime('-'.$day.' days'));
-            $week_end = date('Y-m-d 23:59:59', strtotime('+'.(6-$day).' days'));
+            //Data din momentul rularii:
+            // $dataSetataStart = DateTime::createFromFormat('Y-m-d H:i',date("Y-m-d H:i", strtotime("now")));
+            $dataSetataStart = new DateTime('now');
+            $anAcum = $dataSetataStart->format('Y');
+            $lunaAcum = $dataSetataStart->format('m');
+            $ziAcum = $dataSetataStart->format('w');
+            $oraAcum = $dataSetataStart->format('H');
+            $minuteAcum = $dataSetataStart->format('i');
 
-            $luna = date('M Y');
-            $info['tabelLunaProgram'] = '<div class="programari__calendar__month" id="selected_month">' . strtoupper($luna) . '</div>';
-            $status = "open";
+            //Data de la care trebuie sa incepem in general 
+            $anStart = $anAcum;
+            $lunaStart = $lunaAcum;
+            $ziStart = $ziAcum;
+            $oraStart = $oraAcum;
+
+            //in general alegem saptama curenta
+            $week_start = date('Y-m-d 00:00:00', strtotime('-'.$ziAcum.' days'));
+            $week_end = date('Y-m-d 23:59:59', strtotime('+'.(6-$ziAcum).' days'));
+            
+            if($dataStart != "")
+            {
+                //Este setat un alt start decat cel curent -> alegem aceasta saptamana si ii extragem datele
+                // $dataSetataStart =  DateTime::createFromFormat('Y-m-d H:i', date("Y-m-d H:i", strtotime($anStart ."-".$lunaStart."-".$ziStart." 00:00")));
+                
+                if (DateTime::createFromFormat('Y-m-d', $dataStart) !== false)
+                {
+                    $dataSetataStart =  new DateTime($dataStart);
+                    $anStart = $dataSetataStart->format('Y');
+                    $lunaStart = $dataSetataStart->format('m');
+                    $ziStart = $dataSetataStart->format('w');
+                    $oraStart = $dataSetataStart->format('H');
+                        
+                    $week_start = date('Y-m-d 00:00:00', strtotime('-'.$ziStart.' days'));
+                    $week_end = date('Y-m-d 23:59:59', strtotime('+'.(6-$ziStart).' days'));
+                }
+            }
+            $lunaTabel = $dataSetataStart->format('M Y');
+            $info['tabelLunaProgram'] = '<div class="programari__calendar__month" id="selected_month">' . strtoupper($lunaTabel) . '</div>';
+            
             // print_r("Inceput saptamna = " . $week_start . " sfarsit = " . $week_end);
             // Inceput saptamna = 2022-05-15 00:00:00 sfarsit = 2022-05-21 23:59:59
 
@@ -28,41 +62,53 @@
             // print_r($programariSaptamnaCurenta);
             // Array ( [0] => Array ( [id_appointment] => 3 [date] => 2022-05-20 09:00:00 [status] => 1 [id_user] => 4 [id_form] => 3 ) [1] => Array ( [id_appointment] => 43 [date] => 2022-05-16 18:00:00 [status] => 1 [id_user] => 3 [id_form] => 43 ) [2] => Array ( [id_appointment] => 62 [date] => 2022-05-21 11:00:00 [status] => 1 [id_user] => 2 [id_form] => 62 ) [3] => Array ( [id_appointment] => 67 [date] => 2022-05-15 15:00:00 [status] => 1 [id_user] => 3 [id_form] => 67 ) )
 
+            //pentru fiecare zi adaugam celule cu capul de tabel(zile si luna)
+            for ($i = 0; $i < 7; $i++) 
+            {
+                // $curentDay = DateTime::createFromFormat('Y-m-d', date("Y-m-d", strtotime("+".($i-$ziAcum + 1)." days")));
+                $curentDay = $dataSetataStart->format('D d/m');
+                $info['tabelSaptamanaProgram'] = $info['tabelSaptamanaProgram'] . 
+                '
+                    <div class="programari__calendar__inside__head">
+                        <div class="programari__calendar__inside__day" id="day_' . $i . '_programare">' . strtoupper($curentDay) . '</div>
+                    </div>
+                ';
+            }
+
+            //pentru fiecare ora din program
+            for ($j = 8; $j < 20; $j++) 
+            {
+                //pentru fiecare zi adaugam celule cu continutul tabelului de programare
+                for ($i = 0; $i < 7; $i++) 
+                {
+                    $oraCurentaTabel = DateTime::createFromFormat('Y-m-d H', date("Y-m-d H", strtotime("+" . (($i-$ziAcum + 1)*24*60 - $oraAcum*60 + $j*60 - $minuteAcum)." minutes")));
+                    $oraCurentaFormataTabel = strtoupper(date_format($oraCurentaTabel, 'H:i')); 
+                    $status = "open";
+                    foreach($programariSaptamnaCurenta as $programare)
+                    {
+                        $difenta = $oraCurentaTabel->diff(new DateTime($programare['date']));
+                        if ($difenta->d == 0 && $difenta->h == 0)
+                        {
+                            $status = "busy";
+                        }
+
+                    }
+                    $info['tabelProgram'] = $info['tabelProgram']. 
+                    '
+                        <div class="programari__calendar__inside__hours_btn">
+                            <button type="button" class="programari__calendar__inside__hours_btn__' . $status . '" name="button_progrmare_ora" value="08:00" id="calendar_row' . $i . '_col' . $j . '"> ' . $oraCurentaFormataTabel . '</button>
+                        </div>
+                    ';
+                }
+            }
+
+
             //TO DO : verificare si in sesion si pagina de Referer 
             if ($userName == "")
             {
                 $info['generalbar'] = str_replace("CLIENT_NELOGAT" , $userName , BARA_CLIENT_NELOGAT);
                 $info['butoane'] = '
                     <button type="submit" class="programari__btn__btn1" id="programari__btn__book" name="calendar_action" value="Book"> Book</button>';
-                
-                //pentru fiecare zi
-                for ($i = 0; $i < 7; $i++) 
-                {
-                    $curentDay = DateTime::createFromFormat('Y-m-d', date("Y-m-d", strtotime("+".($i-$day + 1)." days")));
-                    $info['tabelSaptamanaProgram'] = $info['tabelSaptamanaProgram'] . 
-                    '
-                        <div class="programari__calendar__inside__head">
-                            <div class="programari__calendar__inside__day" id="day_' . $i . '_programare">' . strtoupper(date_format($curentDay, 'D d/m')) . '</div>
-                        </div>
-                    ';
-                    //pentru fiecare ora din program
-                    for ($j = 8; $j < 20; $j++) 
-                    {
-                        
-                        $ora = date('h');
-                        $oraCurenta = DateTime::createFromFormat('Y-m-d h', date("Y-m-d h", strtotime("-".($ora)." hours")));
-                        print_r("\n" . date_format($oraCurenta, 'd:00'));
-
-                        $info['tabelProgram'] = $info['tabelProgram']. 
-                        '
-                            <div class="programari__calendar__inside__hours_btn">
-                                <button type="button" class="programari__calendar__inside__hours_btn__' . $status . '" name="button_progrmare_ora" value="08:00" id="calendar_row' . $i . '_col' . $j . '"> 08:00</button>
-                            </div>
-                        ';
-                    }
-                }
-
-                
                 $this->view('programare/index', $info);
             }
             else
